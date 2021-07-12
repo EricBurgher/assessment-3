9104ca3e71ea6ede8b2d3c9a04a2f259fda77be2
Eric Burgher <ericburgher@Erics-Air-2.attlocal.net>
2021-07-12 06:13:39 -0500

    Assessment 3 incomplete

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,24 @@
 -- Schema creation goes here
+CREATE TABLE customer (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(20),
+  last_name VARCHAR(20),
+  email_address VARCHAR(45)
+);
+
+CREATE TABLE product (
+  id PRIMARY KEY,
+  blender VARCHAR(22)
+);
+
+CREATE TABLE category (
+  id PRIMARY KEY,
+  home VARCHAR(33),
+  kitchen VARCHAR(33),
+  bed_and_bath VARCHAR(33),
+  office VARCHAR(33)
+);
+
+CREATE TABLE order_info (
+  id PRIMARY KEY,
+)
--- database/seeds.sql
+++ database/seeds.sql
@@ -1,1 +1,3 @@
 -- Seed data goes here
+INSERT INTO customer (first_name, last_name, email_address)
+VALUES 'Bugs', 'Bunny', 'BugsBunny@gmail.com'
